05013595c6d8c21aa24eaae4f4b83a333e919f7c
Behnam Sheykhe <learndlt@gmail.com>
2020-04-13 00:39:23 +0430

    edit some refrence

--- playground/index.js
+++ playground/index.js
@@ -1,26 +1,3 @@
-// setTimeout(() => {
-//   console.log("tow second is up");
-// }, 2000);
-
-// const names = ["Andrew", "Behnam", "Leamon", "lisa"];
-
-// const shortNames = names.filter((name) => {
-//   return name.length <= 4;
-// });
-
-// const geocode = (address, callback) => {
-//   setTimeout(() => {
-//     const data = {
-//       lat: 0,
-//       lon: 0,
-//     };
-//     callback(data);
-//   }, 2000);
-// };
-
-// geocode("Mahabad", (data) => {
-//   console.log(data);
-// });
 
 const add = (a, b, callback) => {
   setTimeout(() => {
