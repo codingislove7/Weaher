8674f849cfa8061ca48588471eadef7ddbf240cc
Behnam Sheykhe <learndlt@gmail.com>
2020-04-12 07:20:18 +0430

    add error handler

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const request = require("request");
 
 const url =
-  "https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=e057d488da8a4a3b916a074965521ff8";
+  "https://api.weatherbit.io/v2.0/current?key=e057d488da8a4a3b916a074965521ff8&city=Mahabad";
 
 request({ url: url }, (error, response) => {
   const datas = JSON.parse(response.body);
   const array = datas.data;
-  let data;
-  for (arr of array) {
-    data = arr;
+
+  if (error) {
+    console.log("cant access to wearher service!");
+  } else if (datas.error) {
+    console.log("unable to find location");
+  } else {
+    let data;
+    for (arr of array) {
+      data = arr;
+    }
+    console.log(
+      data.weather.description +
+        ".  it is currently " +
+        data.temp +
+        " Celcius degrees out. There is " +
+        data.precip +
+        " mm/hr precipitation rate"
+    );
   }
-  console.log(data);
 });
