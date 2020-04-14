0ea5606b363c16f765c8f1a355e8aa941e0ca3b4
Behnam Sheykhe <learndlt@gmail.com>
2020-04-14 23:37:54 +0430

    add 404 Handeling

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,17 @@
 const path = require("path");
 const express = require("express");
-
-const publicDirectoryPath = path.join(__dirname, "../public");
-
 const app = express();
+const hbs = require("hbs");
+//Define paths  for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//Setup handelbars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
@@ -25,14 +32,27 @@ app.get("/help", (req, res) => {
     title: "help page",
     message:
       "it is simple, just enter your city and we tell you how weather is!",
+    name: "Behnam",
   });
 });
 app.get("/weather", (req, res) => {
   res.send({
-    location: "Mahabad",
+    location: "NewYork",
     forecast: 56,
   });
 });
-app.listen(3008, () => {
-  console.log("Server is up on port 3008.");
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Helparicle not found",
+    name: "Behnam",
+  });
+});
+app.get("*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Page not found. 404",
+    name: "Behnam ",
+  });
+});
+app.listen(3010, () => {
+  console.log("Server is up on port 3010.");
 });
